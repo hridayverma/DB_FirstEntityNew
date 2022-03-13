2a9db5613de984470e1747c0faa888fc32d514fa
hridayverma <hrid.06@gmail.com>
2022-03-13 13:46:12 +0530

    Add project files.

---
+++ DB_FirstEntity/Controllers/StateMgtController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DB_FirstEntity.Controllers
+{
+   public class  Test{
+        public int Id { get; set; }
+    }
+    public class StateMgtController : Controller
+    {
+        // GET: StateMgt
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Data from viewdata";
+            ViewData["Test"] = new Test() { Id=2000};
+            //Writing data in tempdata
+            TempData["mykey"] = "data from temp data";
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            //Reading data from tempdata
+            ViewBag.MyKey1 = TempData["mykey"];
+            TempData.Keep("mykey");
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            //Reading data from tempdata
+            ViewBag.MyKey2 = TempData["mykey"];
+            TempData.Keep("mykey");
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ DB_FirstEntity/Models/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DB_FirstEntity.Models
+{
+    //DTO
+    class EmployeeDetails
+    {
+        //employee poco
+        public int EId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        //Address poco
+        public string AddressLine { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
